5ad07fe96d0424032d5acb8bcaeb5d8995aba3fd
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2023-03-05 13:42:17 +0100

    Update browser.js

--- browser.js
+++ browser.js
@@ -1,9 +1,17 @@
-(function () {
-  function printColor(color) {
-    const fontSize = 'font-size: 14px;';
-    console.log(`%c${color}`, `color:#fff;background-color:${color};${fontSize}`);
-  }
+import Color from './color.js';
 
-  window.utls = window.utls || {};
-  window.utls.color = printColor;
-})();
+function getContrastText(background) {
+  const lightText = '#fff';
+  const darkText = 'rgba(0, 0, 0, 0.87)';
+
+  return Color(background).isLight() ? darkText : lightText;
+}
+
+function printColor(color) {
+  const fontSize = 'font-size: 14px;';
+  const contrast = getContrastText(color);
+  console.log(`%c${color}`, `color:${contrast};background-color:${color};${fontSize}`);
+}
+
+window.u = window.u || {};
+window.u.color = printColor;
