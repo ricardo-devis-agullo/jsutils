b63ed5636d498f28e87d631095db981a7a817f2e
Ricardo Devis Agullo <mkredz@gmail.com>
2023-03-05 14:02:46 +0100

    print obj colors

--- index.js
+++ index.js
@@ -7,11 +7,35 @@ function getContrastText(background) {
   return Color(background).isLight() ? darkText : lightText;
 }
 
-function printColor(color) {
+function printColor({ msg, color }) {
   const fontSize = 'font-size: 14px;';
   const contrast = getContrastText(color);
-  console.log(`%c${color}`, `color:${contrast};background-color:${color};${fontSize}`);
+  if (msg) {
+    console.log(
+      `%c${msg} %c${color}`,
+      fontSize,
+      `color:${contrast};background-color:${color};${fontSize}`
+    );
+  } else {
+    console.log(`%c${color}`, `color:${contrast};background-color:${color};${fontSize}`);
+  }
+}
+
+function printColorObject(obj) {
+  Object.entries(obj)
+    .filter(([, value]) => typeof value === 'string')
+    .forEach(([key, value]) => {
+      printColor({ msg: key, color: value });
+    });
+}
+
+function printColors(data) {
+  if (typeof data === 'string') {
+    printColor({ color: data });
+  } else if (data && typeof data === 'object') {
+    printColorObject(data);
+  }
 }
 
 window.u = window.u || {};
-window.u.color = printColor;
+window.u.color = printColors;
